2760f56a66e6433b9024fc2bf7d8c1fcd752f30e
Anand J <anandj@cs.toronto.edu>
2021-10-27 04:39:52 -0400

    Add Select, Where, Join, and Sum queries in Quilt

--- include/tilt/builder/quilt.h
+++ include/tilt/builder/quilt.h
@@ -10,24 +10,33 @@ using namespace std;
 namespace tilt::tilder {
 
 class QuiltCtx {
+public:
+    QuiltCtx() : count(0) {}
+
 private:
+    vector<Sym> inputs;
     map<Sym, Expr> syms;
+    int count;
 
     friend class Quilt;
 };
 
 class Quilt {
 public:
-    Quilt(shared_ptr<QuiltCtx> ctx) : ctx(ctx) {}
-
-    Op build();
+    static Quilt Stream(string, DataType, shared_ptr<QuiltCtx>);
+    Op Build();
 
     Quilt Select(function<Expr(_sym)>);
     Quilt Where(function<Expr(_sym)>);
-    Quilt Join(Quilt, function<Expr(_sym, _sym)>);
+    Quilt InnerJoin(Quilt, function<Expr(_sym, _sym)>);
+    Quilt Sum(int64_t, int64_t);
 
 private:
-    Sym sym;
+    Quilt(_sym sym, shared_ptr<QuiltCtx> ctx) : sym(sym), ctx(ctx) {}
+
+    Quilt submit(string, Op);
+
+    _sym sym;
     shared_ptr<QuiltCtx> ctx;
 };
 
